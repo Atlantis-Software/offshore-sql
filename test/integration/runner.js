73a677a64ac37e9dd63bfbe7171049b5281e0562
Alexandre Tiertant <a.tiertant@atlantis-software.net>
2016-03-03 15:03:43 +0100

    fix database param

--- test/integration/runner.js
+++ test/integration/runner.js
@@ -69,7 +69,7 @@ new TestRunner({
     host: process.env.OFFSHORE_ADAPTER_TESTS_OFFSHORE_SQL_HOST || '127.0.0.1',
     user: process.env.MYSQL_ENV_MYSQL_USER || process.env.OFFSHORE_ADAPTER_TESTS_OFFSHORE_SQL_USER || 'root',
     password: process.env.MYSQL_ENV_MYSQL_PASSWORD || process.env.OFFSHORE_ADAPTER_TESTS_OFFSHORE_SQL_PASSWORD || '',
-    database: process.env.MYSQL_ENV_MYSQL_DATABASE || process.env.OFFSHORE_ADAPTER_TESTS_OFFSHORE_SQL_DB||'offshoresql'
+    database: process.env.MYSQL_ENV_MYSQL_DATABASE || process.env.OFFSHORE_ADAPTER_TESTS_OFFSHORE_SQL_DB|| 'offshoreSql'
   },
 
   // The set of adapter interfaces to test against.
